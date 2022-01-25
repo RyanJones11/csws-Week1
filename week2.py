6c668ea58cc7ec88cd917dcc153b88f89e6df06b
ryanajones1@hotmail.co.uk <ryanajones1@hotmail.co.uk>
2022-01-25 13:48:29 +0000

    Implemented task from 3-8

--- week2.py
+++ week2.py
@@ -1,3 +1,9 @@
-Names = ["Ryan", "Harry", "Kyle","Andy", "Owen"]
-for i in Names:
-    print("Hello "+i+"!")
+Places = ["SF", "Greece", "Canada", "Amsterdam", "LA"]
+print(Places)#Prints list in original state
+print(sorted(Places))#Prints list in alphabetical order
+print(sorted(Places, reverse=True))#Prints list in reverse alphabetical order
+print(Places)#prints in original state to show not permanently sorted
+Places.reverse()#Reverses the original list
+print(Places)#Prints the list now reversed
+Places.reverse()#Reverses the list to the original
+print(Places)#Prints the list reversed again
